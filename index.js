2eab8457b51ffda3864a43f9b1d8eee07bd11f25
afd <afd@alup5.academiapostal.es.local>
2020-01-24 20:01:12 +0100

    carrito ok

--- index.js
+++ index.js
@@ -5,8 +5,17 @@
  * 
  * @param {array de objetos} carrito 
  */
-export function calcularTotal(carrito) {
-  return 0;
+export function calcularPeso(carrito){
+
+return 0
 }
-
-
+export function calcularTotal(carrito){
+  var total=0;
+  for(var i=0; i<carrito.length; i++){
+    if(carrito[i] .cantidad){
+    total+= carrito[i] .precio * carrito[i] .cantidad;}
+    else {
+      total += carrito[i] .precio}
+  }
+  return total;
+}
--- test.js
+++ test.js
@@ -1,6 +1,21 @@
-import { toRoman } from './index';
+import { calcularTotal, calcularPeso } from './index';
 import { equal } from "assert";
+
 
 describe(('Cálculo del total'), () => {
   it('carrito vacío', () => { equal(calcularTotal([]), 0) })
+  it('Comprar Pan', () => { equal(calcularTotal([{producto:"Pan", precio:1}]), 1)});
+  it('Comprar Pan y leche', () => { equal(calcularTotal([
+    {producto:"Pan", precio:1},
+    {producto:"Leche", precio:1}
+  ]), 2)});
+  it('Comprar Huevo', () => { equal(calcularTotal([
+    {producto:"Huevo", precio:30, cantidad:12}]),360)});
+
+    describe(('Cálculo del peso'), () => {
+      it('carrito vacío', () => { equal(calcularPeso([]), 0) })
+ 
+
+    });
+
 })
